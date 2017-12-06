627979f6365acc9cb21ba996cabb5e318bbe6f04
Johannes Scribante <johannesscr@Johanness-MacBook.local>
2017-12-06 09:46:12 +0200

    feat - added the .gitignore file and start-up comments for the linear_regression.py file.

---
+++ linear_regression.py
@@ -0,0 +1,10 @@
+# MACHINE LEARNING FROM FIRST PRINCIPALS
+
+"""
+LINEAR REGRESSION
+
+Based on theory and first principals, the aim is to build simple linear
+regression model, to understand how linear regression works and how
+one would go about implementing it, should there ever be a need to that
+a library does not support.
+"""
